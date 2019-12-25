11af5b2f374f40890b75f200fd9d3016e21275b9
42awa <gqozgak1@gmail.com>
2019-12-25 16:03:58 +0200

    test

--- src/com/isprogramming/Chart.java
+++ src/com/isprogramming/Chart.java
@@ -14,6 +14,7 @@ import java.sql.*;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 
 public class Chart extends JFrame{
     private JPanel panel1;
@@ -22,11 +23,14 @@ public class Chart extends JFrame{
     private static Connection con;
     private static Statement stmt;
     private static ResultSet rs;
+    private static ResultSet rs1;
+    private static Statement stmt1;
     Chart() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/isproject?characterEncoding=latin1", "root", "12345678");
         stmt = con.createStatement();
+        stmt1 = con.createStatement();
         setTitle("Tasks And SubTasks");
         setSize(800, 600);
         add(panel1);
@@ -48,19 +52,46 @@ public class Chart extends JFrame{
     }
     private IntervalCategoryDataset getCategoryDataset() throws SQLException {
         TaskSeriesCollection dataset = new TaskSeriesCollection();
-        rs=stmt.executeQuery("select t_name,startDate,dueDate from task left JOIN suptasks\n"+
-                "ON task.t_id = suptasks.t_id\n"+
-                "GROUP BY t_name;\n");
-        TaskSeries series1 = new TaskSeries("Actual Date");
+        ArrayList<Integer>tasksID=new ArrayList<>();
+        rs=stmt.executeQuery("select t_name,startDate,dueDate,t_id from task");
+
+        TaskSeries series1 = new TaskSeries("expected Date");
         while (rs.next()){
-            LocalDate date = rs.getDate(2).toLocalDate();
-            Instant i = date.atStartOfDay().toInstant(ZoneOffset.UTC);
-            LocalDate datee = rs.getDate(3).toLocalDate();
-            Instant ii = datee.atStartOfDay().toInstant(ZoneOffset.UTC);
-            series1.add(new Task(rs.getString(1),
-                    Date.from(i),
-                    Date.from(ii)
-            ));
+            rs1=stmt1.executeQuery("select supTask_id from suptasks where supTask_id="+rs.getInt(4));
+            if(!rs1.next()){
+                LocalDate date = rs.getDate(2).toLocalDate();
+                Instant i = date.atStartOfDay().toInstant(ZoneOffset.UTC);
+                LocalDate datee = rs.getDate(3).toLocalDate();
+                Instant ii = datee.atStartOfDay().toInstant(ZoneOffset.UTC);
+                series1.add(new Task(rs.getString(1),
+                        Date.from(i),
+                        Date.from(ii)
+                ));
+                tasksID.add(rs.getInt(4));
+            }
+
+        }
+
+        TaskSeries series2 = new TaskSeries("Actual Date");
+        for(int i=0;i<tasksID.size();i++){
+            rs1=stmt1.executeQuery("select actualHours from actualtasks where t_id="+tasksID.get(i));
+            if(rs1.next()){
+                int hour=rs1.getInt(1);
+                int days=hour/24;
+                System.out.println(tasksID.get(i));
+                rs=stmt.executeQuery("select t_name,startDate,dueDate from task where t_id="+tasksID.get(i));
+                LocalDate date = rs.getDate(2).toLocalDate();
+                Instant e = date.atStartOfDay().toInstant(ZoneOffset.UTC);
+                LocalDate datee = rs.getDate(3).toLocalDate();
+                datee.plusDays(days);
+                Instant ee = datee.atStartOfDay().toInstant(ZoneOffset.UTC);
+                series1.add(new Task(rs.getString(1),
+                        Date.from(e),
+                        Date.from(ee)
+                ));
+            }
+
+
         }
         /*series1.add(new Task("Requirement",
                 Date.from(LocalDate.of(2017, 7, 3).atStartOfDay().toInstant(ZoneOffset.UTC)),
@@ -116,7 +147,7 @@ public class Chart extends JFrame{
 
 
         dataset.add(series1);
-//        dataset.add(series2);
+        dataset.add(series2);
         return dataset;
     }
 
